5678983ffeb842870fd440567d2940af3f800603
Vitali <findrsen@gmail.com>
2021-02-06 17:02:58 +0200

    fix

--- src/comm.js
+++ src/comm.js
@@ -1,17 +1,18 @@
 import { exec } from 'child_process'
 import readlineSync from 'readline-sync'
 
-const execRun = (command) => {
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`)
-      return
-    } else if (stderr) {
-      console.log(`stderr: ${stderr}`)
-      return
-    }
-    console.log(`stdout: ${stdout}`)
-  })
+const execRun = (command, delay = 1000) => {
+  setTimeout(function () {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`)
+        return
+      } else if (stderr) {
+        console.log(`stderr: ${stderr}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+    })}, delay)
 }
 
 export default () => {
