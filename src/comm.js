e9a874178ca15232b5c626d2945cfd80a85dca04
Vitali <findrsen@gmail.com>
2021-02-06 15:01:42 +0200

    changed commit command

--- src/comm.js
+++ src/comm.js
@@ -1,31 +1,25 @@
 import { exec } from 'child_process'
 import readlineSync from 'readline-sync'
 
+const execRun = (command) => {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`)
+      return
+    }
+    else if (stderr) {
+      console.log(`stderr: ${stderr}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+  })
+}
+
 export default () => {
   const commitName = String(readlineSync.question('Commit name: '))
 
   if (commitName !== 'stop') {
-    exec(`git add .`, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`)
-        return
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`)
-        return
-      }
-      console.log(`stdout: ${stdout}`)
-    })
-    exec(`git commit -m '${commitName}'`, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`)
-        return
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`)
-        return
-      }
-      console.log(`stdout: ${stdout}`)
-    })
+    execRun('git add .')
+    execRun(`git commit -m '${commitName}'`)
   }
 }
